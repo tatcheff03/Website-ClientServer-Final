14dfdceb76fa33098a5f5c764d96ab60fd96b581
Angel Tatcheff <f109309@students.nbu.bg>
2025-01-22 15:19:24 +0200

    Committed Database file

---
+++ Website ClientServer/Models/ResetPasswordWithoutEmailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    //custom view model with required fields for reset password
+    public class ResetPasswordWithoutEmailViewModel
+    {
+        //email field required
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        //pass field required
+        [Required(ErrorMessage = "New password is required.")]
+        //max 100 characters
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string NewPassword { get; set; }
+    }
+}
